46060df0cbfd50ae6663e2362dd8ef3e2e99255c
julian <julenposper@outlook.com>
2026-02-27 14:21:57 -0500

    añadido imagen del logo y algunos cambios en el header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
+import logo from '../assets/antt.png'
+
 function Navbar() {
-    return(
-        <header style={{
-            background:"#333",
-            color: "#fff",
-            padding: "1rem"
-        }}>
-            <h1>Ant Track</h1>
-        </header>
-    )
+  return (
+    <header style={{ 
+      background: '#333', 
+      color: '#fff', 
+      padding: '1rem',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+      <img 
+        src={logo} 
+        alt="Logo App Ant Track" 
+        style={{ height: '60px' }} 
+      />
+      <h1 style={{ marginTop: '0.5rem' }}>App Ant Track</h1>
+    </header>
+  )
 }
+
 export default Navbar
