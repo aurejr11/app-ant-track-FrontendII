4fa710e5f8c83aa9809b53e90ef53b012ec803a8
Maricela <correo_ochoamaricela22@gmail.com>
2026-03-06 03:54:57 -0500

    Feat: Estructura de diseño con Flexbox

--- componente_5/src/App.jsx
+++ componente_5/src/App.jsx
@@ -4,7 +4,34 @@ function App(){
 
     return(
       <>
-        <Services />
+ 
+    <div className="app-container">
+      <aside className="sidebar">
+        <p>COMPONENTE UNO</p>
+      </aside>
+
+      <main className="main-content">
+        <section className="banner">
+          <p>COMPONENTE DOS</p>
+        </section>
+
+        <section className="cards-row">
+          <div className="card">
+            <div className="card-body">COMPONENTE TRES</div>
+            <div className="card-footer">TÍTULO</div>
+          </div>
+           <div className="card">
+            <div className="card-body">COMPONENTE TRES</div>
+            <div className="card-footer">TÍTULO</div>
+          </div>
+           <div className="card">
+            <div className="card-body">COMPONENTE TRES</div>
+            <div className="card-footer">TÍTULO</div>
+          </div>
+        </section>
+      </main>
+    </div>
+
 
       </>
     )
